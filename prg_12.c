26abb7271e894dfe10af0ab780f147413e37189f
BHADRA <bhadrabinoj2004@gmail.com>
2025-08-05 20:39:17 +0530

    Add files via upload

---
+++ prg_12.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#define SIZE 5
+int stack[SIZE];
+int top = -1;
+void push(int value) 
+{
+    if (top == SIZE - 1) 
+	{	
+        printf("Stack Overflow\n");
+    } else {
+        top++;
+        stack[top] = value;
+        printf("%d pushed to stack\n", value);
+    }
+}
+void pop() 
+{
+    if (top == -1) 
+	{
+        printf("Stack Underflow\n");
+    } else {
+        printf("%d popped from stack\n", stack[top]);
+        top--;
+    }
+}
+void peek() {
+    if (top == -1) 
+	{
+        printf("Stack is empty\n");
+    } else {
+        printf("Top element is %d\n", stack[top]);
+    }
+}
+void display() 
+{
+	int i;
+    if (top == -1) 
+	{
+        printf("Stack is empty\n");
+    } else {
+        printf("Stack elements: ");
+        for ( i = top; i >= 0; i--) 
+		{
+            printf("%d ", stack[i]);
+        }
+        printf("\n");
+    }
+}
+int main() {
+    int choice, value;
+    do 
+	{
+        printf("\n1. Push\n2. Pop\n3. Peek\n4. Display\n5. Exit\nEnter your choice: ");
+        scanf("%d", &choice);
+        switch (choice) 
+		{
+        case 1:
+            printf("Enter value to push: ");
+            scanf("%d", &value);
+            push(value);
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            peek();
+            break;
+        case 4:
+            display();
+            break;
+        case 5:
+            printf("Exiting...\n");
+            break;
+        default:
+            printf("Invalid choice\n");
+        }
+    } 
+	while (choice != 5);
+    return 0;
+}
+
